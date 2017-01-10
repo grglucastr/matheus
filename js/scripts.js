c2644f3eb4cc3d8a36de7bc76faa3f96124bc14e
George Lucas T. Bentes <george.bentes@gmail.com>
2017-01-10 15:12:10 -0400

    Ajustments, single page, menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    //$('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        menu: '#menu',
+        anchors:['home','identidades-visuais', 'campanhas', 'contato']
+    });
+    $(".button-collapse").sideNav();
 });
