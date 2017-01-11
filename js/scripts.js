ee0551fe19dc255d1292c2e9ad6311e8fba69d51
George Lucas T. Bentes <george.bentes@gmail.com>
2017-01-10 21:52:00 -0400

    Ajustments: menu, scroll, backgrounds, margins

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,28 @@ $(document).ready(function() {
     //$('#fullpage').fullpage();
     $('#fullpage').fullpage({
         menu: '#menu',
-        anchors:['home','identidades-visuais', 'campanhas', 'contato']
+        anchors:['homepage','identidades-visuais',
+        'campanhas', 'anuncios', 'websites',
+        'redes-sociais', 'contato'],
+
+        onLeave: function(index, nextIndex, direction){
+            var leavingSection = $(this);
+
+            //after leaving section 1
+            if(index == 1 && direction =='down'){
+                if($("#menu").hasClass('main-menu-regular')){
+                    $("#menu").removeClass('main-menu-regular');
+                }
+                $("#menu").addClass('main-menu-floating');
+                console.log('Add class');
+
+            }else if(index == 2 && direction == 'up'){
+                $("#menu").removeClass('main-menu-floating');
+                $("#menu").addClass('main-menu-regular');
+                console.log('remove class');
+            }
+        }
     });
     $(".button-collapse").sideNav();
+    console.log(`Width: ${screen.width}`);
 });
