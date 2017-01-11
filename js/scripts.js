83ce217235a2c42194e7ea3eb9d1f8513f3841a3
George Lucas T. Bentes <george.bentes@gmail.com>
2017-01-11 10:57:44 -0400

    Added images gallery template

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
+
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer'
+    });
+
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry();
+    });
+
     //console.log(`Width: ${screen.width}`);
 });
